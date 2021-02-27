c79c61bc433ac3a332ed1b33b777bc6e6fbf2bfc
BartelKarlijn <67980453+BartelKarlijn@users.noreply.github.com>
2021-02-27 10:18:44 +0100

    first commit

---
+++ src/configuration.h
@@ -0,0 +1,8 @@
+// Configuration file
+
+// Pins motor
+#define leftMotorPWMPin   6
+#define leftMotorDirPin   11
+#define rightMotorPWMPin  5
+#define rightMotorDirPin  3
+
---
+++ src/main/include.h
@@ -0,0 +1,38 @@
+// Arduino library includes
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <math.h>
+#include <analogWrite.h>
+#include <ESP32TimerInterrupt.h>
+
+// //Configuratie
+#include <configuration.h>
+// #include <main/globalvariables.h>
+
+// // define
+// #include <main/arraydefine.h>
+
+// // eigen subprogrammas
+// #include <main/printdebug.h>
+// #include <wifi/wifidefinitions.h>
+// #include <wifi/htmlbutton.h>
+// #include <wifi/sendhtml.h>
+// #include <wifi/handle.h>
+// #include <wifi/setupwifi.h>
+// #include <pixels/setcolor.h>
+// #include <pixels/pixelneon.h>
+// #include <pixels/pixeldrl.h>
+// #include <pixels/pixelsweep.h>
+// #include <pixels/pixelprocessdim.h>
+// //#include <pixels/pixeldim.h>
+// //#include <pixels/pixelstop.h>
+// //#include <pixels/pixelpinkersweep.h>
+// #include <pixels/pixeltest.h>
+// #include <pixels/showpixels.h>
+// #include <main/setuppins.h>
+// #include <main/setupleds.h>
+
