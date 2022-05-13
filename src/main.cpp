92aa6aff98393340f56db4b239478c6da303bca0
BartelKarlijn <legoreus@gmail.com>
2022-05-13 16:09:26 +0200

    even zonder MPU, die verhinderde om in loop te komen

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ void setup() {
   #ifdef flag_calibrateMPU 
     calibrateMPUsetup();  // als je MPU wil callibreren
   #else
-    setupMPU();           // MPU en de DMP opstarten
+//    setupMPU();           // MPU en de DMP opstarten
   #endif
 
   loop_timer = micros() + PERIOD;
